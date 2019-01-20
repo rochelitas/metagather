e7562607b665e7d7c5d2d386a7182f3f61487d12
Lev Babchenko <lvbabchenko@yandex-team.ru>
2019-01-20 20:46:56 +0300

    fix run script; add __pycache__ to ignores

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
+set -e
+
 cd $(dirname $(readlink -f "$0"))
 if [ ! -d ./venv ]; then
   virtualenv --python=python3 ./venv
   (
-    source ./venv/bin/initialize
+    source ./venv/bin/activate
     pip install -r ./required
   )
 fi
 
-source ./venv/bin/initialize
+source ./venv/bin/activate
 python run-merge.py
